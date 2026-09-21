7283092a911aaddbb121dc084f2723a88ea94348
sitrakiniainadiary15-sketch <sitrakiniainadiary15@gmail.com>
2026-09-21 14:37:03 +0300

    chnagement logo logo et favicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -13,7 +13,7 @@ export default function manifest(): MetadataRoute.Manifest {
     icons: [
       {
         src: "/icon.png",
-        sizes: "48x48",
+        sizes: "192x192",
         type: "image/png",
       },
       {
